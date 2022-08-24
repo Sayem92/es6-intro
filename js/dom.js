3006ddb3c1f87e552fbb7efdb6d154a1a1a20ef4
Sayem92 <sayemmiha123@gmail.com>
2022-08-24 11:33:22 +0600

    recap js

---
+++ js/dom.js
@@ -0,0 +1,25 @@
+document.getElementById('apply-bg').addEventListener('click',function(){
+    const friends = document.getElementsByClassName("friend")
+    for(const friend of friends){
+        friend.style.backgroundColor = 'lightblue'
+        // console.log(friend)
+    }
+})
+
+
+document.getElementById('center-third').addEventListener('click',function(){
+    const third = document.getElementById('third-friend');
+    third.style.textAlign = 'center'
+    
+});
+
+document.getElementById('add-friend').addEventListener('click', function(){
+    const friendContainer = document.getElementById('friends');
+    const friend = document.createElement('div')
+    friend.classList.add('friend')
+    friend.innerHTML = `
+    <h3 class="friend-name">New friend</h3>
+    <p>something new added</p>
+    `
+    friendContainer.appendChild(friend)
+})
